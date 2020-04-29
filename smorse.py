d4961bc93a0bef155066a2ff70659a0fafd4430b
huffingFreon <higginsba@etsu.edu>
2020-04-29 13:19:47 -0400

    renamed

---
+++ smorse.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# Returns array of 26 morse code letters from the file
+def smorse_ray(fileName):
+    with open(fileName) as f:
+        data = f.readline().split() 
+    return data
+
+# Returns the string passed converted into morse code with no spaces
+def smorse(strToMorse):
+    data = smorse_ray('morse.txt')
+    morsed = ""
+    for letter in strToMorse:
+        morsed += data[ord(letter) - 97]
+    return morsed
+
+userPut = input("What string would you like to smorse: ")
+print(smorse(userPut))
