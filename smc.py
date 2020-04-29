ada15cc5fc05ab1b649b36062b30dd1a9a06155b
huffingFreon <higginsba@etsu.edu>
2020-04-29 13:00:44 -0400

    documentation

--- smc.py
+++ smc.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
+# Returns array of 26 morse code letters from the file
 def smorse_ray(fileName):
     with open(fileName) as f:
         data = f.readline().split() 
     return data
 
+# Returns the string passed converted into morse code with no spaces
 def smorse(strToMorse):
     data = smorse_ray('morse.txt')
     morsed = ""
