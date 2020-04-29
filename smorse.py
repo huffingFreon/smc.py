fef3e423a22fa11a7f42f0e2bf3f3903fb577f72
huffingFreon <higginsba@etsu.edu>
2020-04-29 14:29:18 -0400

    added number_of_occurrences method

--- smorse.py
+++ smorse.py
@@ -14,6 +14,7 @@ def smorse(strToMorse):
         morsed += data[ord(letter) - 97]
     return morsed
 
+# Returns a list of each line in the file smorsed
 def file_smorse(fileName):
     with open(fileName) as f:
         data = f.read().splitlines()
@@ -22,3 +23,14 @@ def file_smorse(fileName):
             morsed.append(smorse(line))
     return morsed
 
+# Returns a dictionary with each unique smorsed string as a key and the value being number of occurrences
+def number_of_occurrences(dataList):
+    morseDict = {}
+    for item in dataList:
+        if item not in morseDict:
+            morseDict[item] = 1
+        else:
+            morseDict[item] += 1
+    return morseDict
+
+print(number_of_occurrences(file_smorse('tiny.txt')))
